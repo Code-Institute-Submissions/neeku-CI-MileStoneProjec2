550183ae505082aa48a691afab9609c2c1538269
Neeku <neeku.sh@gmail.com>
2021-04-30 08:52:45 +0000

    added email.js file

---
+++ assets/js/email.js
@@ -0,0 +1,14 @@
+// https://dashboard.emailjs.com/admin/integration
+emailjs.init('user_150FMuXduK2ftYILsFJpC');
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function () {
+                console.log('SUCCESS!');
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
